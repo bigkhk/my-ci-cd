fa75b13d97b0012bb008083ae43616f71e57e4f9
bigkhk <85100718+bigkhk@users.noreply.github.com>
2025-03-22 21:38:20 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-stdbuf -oL echo "🚀 배포 스크립트 실행 시작"
-stdbuf -oL echo "📦 최신 코드 가져오는 중..."
-stdbuf -oL echo "🔨 애플리케이션 빌드 중..."
-stdbuf -oL echo "🛑 기존 애플리케이션 종료..."
-stdbuf -oL echo "🐳 새로운 애플리케이션 배포 중..."
-stdbuf -oL echo "✅ 배포 완료!"
+echo "🚀 배포 스크립트 실행 시작"
+echo "📦 최신 코드 가져오는 중..."
+echo "🔨 애플리케이션 빌드 중..."
+echo "🛑 기존 애플리케이션 종료..."
+echo "🐳 새로운 애플리케이션 배포 중..."
+echo "✅ 배포 완료!"
